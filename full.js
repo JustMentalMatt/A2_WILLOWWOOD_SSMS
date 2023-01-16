a439bf539553f3c5a4781d87052dc780950be179
JustMentalMatt <matthoey69@hotmail.com>
2023-01-16 18:56:43 +0000

    removal of unnecessary files

--- full.js
+++
@@ -1,4 +0,0 @@
-<div className="flex justify-center items-center flex-row">
-                        <span className={`${red_hat_mono.className} text-white text-4xl mt-12`}>&#62;</span>
-                        <input className={`${red_hat_mono.className} text-white text-4xl mt-12 bg-transparent focus:outline-none ml-4 border-b-2`} value={"hohohoohohoh"} />
-                    </div>
