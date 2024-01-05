e38e938b077fd3a1d8b098a48d37ae6c02c5e4c4
JustMentalMatt <matthoey69@hotmail.com>
2024-01-05 01:31:15 +0000

    added GIF

--- GUI_Design/Login/main.py
+++ GUI_Design/Login/main.py
@@ -5,15 +5,14 @@ app = CTk()
 app.geometry("600x480")
 app.resizable(False,False)
 
-side_img_data = Image.open("./GUI_Design/Login/Images/side-img.png")
+side_img_data = Image.open("./GUI_Design/Login/Images/side-img.gif")
 email_icon_data = Image.open("./GUI_Design/Login/Images/email-icon.png")
 password_icon_data = Image.open("./GUI_Design/Login/Images/password-icon.png")
-google_icon_data = Image.open("./GUI_Design/Login/Images/google-icon.png")
 
 side_img = CTkImage(dark_image=side_img_data, light_image=side_img_data, size=(300, 480))
 email_icon = CTkImage(dark_image=email_icon_data, light_image=email_icon_data, size=(20,20))
 password_icon = CTkImage(dark_image=password_icon_data, light_image=password_icon_data, size=(17,17))
-google_icon = CTkImage(dark_image=google_icon_data, light_image=google_icon_data, size=(17,17))
+
 
 CTkLabel(master=app, text="", image=side_img).pack(expand=True, side="left")
 
@@ -31,7 +30,7 @@ CTkLabel(master=frame, text="  Password:", text_color="#601E88", anchor="w", jus
 CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000", show="*").pack(anchor="w", padx=(25, 0))
 
 CTkButton(master=frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12), text_color="#ffffff", width=225).pack(anchor="w", pady=(40, 0), padx=(25, 0))
-CTkButton(master=frame, text="Continue With Google", fg_color="#EEEEEE", hover_color="#EEEEEE", font=("Arial Bold", 9), text_color="#601E88", width=225, image=google_icon).pack(anchor="w", pady=(20, 0), padx=(25, 0))
+CTkButton(master=frame, text="Register", fg_color="#EEEEEE", hover_color="#c4c2c2", font=("Arial Bold", 9), text_color="#601E88", width=225).pack(anchor="w", pady=(20, 0), padx=(25, 0))
 
 
 app.mainloop()
