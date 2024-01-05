4f63e8e40e9434d1e72b3ad0526e0bd46f10a557
JustMentalMatt <matthoey69@hotmail.com>
2024-01-05 02:34:07 +0000

    adasds

--- GUI_Design/Inventory_Management/order_form.py
+++ GUI_Design/Inventory_Management/order_form.py
@@ -12,34 +12,34 @@ sidebar_frame = CTkFrame(master=app, fg_color="#2A8C55",  width=176, height=650,
 sidebar_frame.pack_propagate(0)
 sidebar_frame.pack(fill="y", anchor="w", side="left")
 
-logo_img_data = Image.open("logo.png")
+logo_img_data = Image.open("./GUI_Design/Inventory_Management/Images/logo.png")
 logo_img = CTkImage(dark_image=logo_img_data, light_image=logo_img_data, size=(77.68, 85.42))
 
 CTkLabel(master=sidebar_frame, text="", image=logo_img).pack(pady=(38, 0), anchor="center")
 
-analytics_img_data = Image.open("analytics_icon.png")
+analytics_img_data = Image.open("./GUI_Design/Inventory_Management/Images/analytics_icon.png")
 analytics_img = CTkImage(dark_image=analytics_img_data, light_image=analytics_img_data)
 
 CTkButton(master=sidebar_frame, image=analytics_img, text="Dashboard", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(60, 0))
 
-package_img_data = Image.open("package_icon.png")
+package_img_data = Image.open("./GUI_Design/Inventory_Management/Images/package_icon.png")
 package_img = CTkImage(dark_image=package_img_data, light_image=package_img_data)
 
 CTkButton(master=sidebar_frame, image=package_img, text="Orders", fg_color="#fff", font=("Arial Bold", 14), text_color="#2A8C55", hover_color="#eee", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
-list_img_data = Image.open("list_icon.png")
+list_img_data = Image.open("./GUI_Design/Inventory_Management/Images/list_icon.png")
 list_img = CTkImage(dark_image=list_img_data, light_image=list_img_data)
 CTkButton(master=sidebar_frame, image=list_img, text="Orders", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
-returns_img_data = Image.open("returns_icon.png")
+returns_img_data = Image.open("./GUI_Design/Inventory_Management/Images/returns_icon.png")
 returns_img = CTkImage(dark_image=returns_img_data, light_image=returns_img_data)
 CTkButton(master=sidebar_frame, image=returns_img, text="Returns", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
-settings_img_data = Image.open("settings_icon.png")
+settings_img_data = Image.open("./GUI_Design/Inventory_Management/Images/settings_icon.png")
 settings_img = CTkImage(dark_image=settings_img_data, light_image=settings_img_data)
 CTkButton(master=sidebar_frame, image=settings_img, text="Settings", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(16, 0))
 
-person_img_data = Image.open("person_icon.png")
+person_img_data = Image.open("./GUI_Design/Inventory_Management/Images/person_icon.png")
 person_img = CTkImage(dark_image=person_img_data, light_image=person_img_data)
 CTkButton(master=sidebar_frame, image=person_img, text="Account", fg_color="transparent", font=("Arial Bold", 14), hover_color="#207244", anchor="w").pack(anchor="center", ipady=5, pady=(160, 0))
 
