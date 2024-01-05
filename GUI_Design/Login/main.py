5b731277b870e0cba30046b17c7eea7ca90a34af
JustMentalMatt <matthoey69@hotmail.com>
2024-01-05 02:13:14 +0000

    format colours

--- GUI_Design/Login/main.py
+++ GUI_Design/Login/main.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 app = CTk()
 app.geometry("600x480")
+app.title("i love jesus better than icecream")
 app.resizable(False,False)
 
 side_img_data = Image.open("./GUI_Design/Login/Images/side-img.gif")
@@ -20,7 +21,7 @@ frame = CTkFrame(master=app, width=300, height=480, fg_color="#ffffff")
 frame.pack_propagate(0)
 frame.pack(expand=True, side="right")
 
-CTkLabel(master=frame, text="Welcome Back!", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 24)).pack(anchor="w", pady=(50, 5), padx=(25, 0))
+CTkLabel(master=frame, text="Welcome Back!", text_color="#37007a", anchor="w", justify="left", font=("Arial Bold", 24)).pack(anchor="w", pady=(50, 5), padx=(25, 0))
 CTkLabel(master=frame, text="Sign in to your account", text_color="#7E7E7E", anchor="w", justify="left", font=("Arial Bold", 12)).pack(anchor="w", padx=(25, 0))
 
 CTkLabel(master=frame, text="  Email:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14), image=email_icon, compound="left").pack(anchor="w", pady=(38, 0), padx=(25, 0))
@@ -29,8 +30,8 @@ CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", bo
 CTkLabel(master=frame, text="  Password:", text_color="#601E88", anchor="w", justify="left", font=("Arial Bold", 14), image=password_icon, compound="left").pack(anchor="w", pady=(21, 0), padx=(25, 0))
 CTkEntry(master=frame, width=225, fg_color="#EEEEEE", border_color="#601E88", border_width=1, text_color="#000000", show="*").pack(anchor="w", padx=(25, 0))
 
-CTkButton(master=frame, text="Login", fg_color="#601E88", hover_color="#E44982", font=("Arial Bold", 12), text_color="#ffffff", width=225).pack(anchor="w", pady=(40, 0), padx=(25, 0))
-CTkButton(master=frame, text="Register", fg_color="#EEEEEE", hover_color="#c4c2c2", font=("Arial Bold", 9), text_color="#601E88", width=225).pack(anchor="w", pady=(20, 0), padx=(25, 0))
+CTkButton(master=frame, text="Login", fg_color="#37007a", hover_color="#E44982", font=("Arial Bold", 12), text_color="#ffffff", width=225).pack(anchor="w", pady=(40, 0), padx=(25, 0))
+CTkButton(master=frame, text="Register", fg_color="#EEEEEE", hover_color="#c4c2c2", font=("Arial Bold", 12), text_color="#601E88", width=225).pack(anchor="w", pady=(20, 0), padx=(25, 0))
 
 
 app.mainloop()
